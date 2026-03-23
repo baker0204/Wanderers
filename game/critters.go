14bd5397ce3e077fc008a09913f129c159863172
baker0204 <bgloften@gmail.com>
2026-03-22 21:40:33 -0500

    Functional critters

--- game/critters.go
+++ game/critters.go
@@ -1,6 +1,10 @@
 package game
 
-import "math/rand"
+import (
+    "fmt"
+    "math"
+    "math/rand"
+)
 
 type Critter struct {
     Name      string
@@ -10,8 +14,8 @@ type Critter struct {
     Hostile   bool
     MoveCount int
 	MoveStyle string
-	Mood string
-	Health int
+	Mood      string
+	Health    int
 }
 
 func (crit *Critter) GetSymbol() rune { return crit.Symbol }
@@ -65,4 +69,128 @@ func NewKappa() *Critter {
     }
 }
 
-type CritterManager struct {}
+type CritterManager struct {
+    Critters []*Critter
+}
+
+func NewCritterManager() *CritterManager {
+    return &CritterManager{
+        Critters: []*Critter{},
+    }
+}
+
+func (cm *CritterManager) Spawn(critter *Critter, grid [][]Tile) {
+    for {
+        x := DiceRoll(len(grid[0])-1)
+        y := DiceRoll(len(grid)-1)
+        if grid[y][x].Walkable && grid[y][x].Entity == nil {
+            critter.X = x  // ← in here
+            critter.Y = y  // ← in here
+            grid[y][x].Entity = critter
+            cm.Critters = append(cm.Critters, critter)
+            break
+        }
+    }
+}
+
+func (cm *CritterManager) MoveAll(grid [][]Tile, party *Party) {
+    for _, crit := range cm.Critters {
+        // move crit based on its MoveStyle
+        Direction := DiceRoll(4)
+        newX := crit.X
+        newY := crit.Y
+        switch crit.MoveStyle {
+            case "random":
+                switch Direction {
+                case 1:
+                    newY++
+                case 2:
+                    newY--
+                case 3:
+                    newX++
+                case 4:
+                    newX--
+                }
+            case "toward":
+                    cx, cy := party.Center()
+                    if cx > crit.X { newX++ } else if cx < crit.X { newX-- }
+                    if cy > crit.Y { newY++ } else if cy < crit.Y { newY-- }
+            case "slow":
+                if DiceRoll(2) == 1 {break} else {
+                    switch Direction {
+                    case 1:
+                        newY++
+                    case 2:
+                        newY--
+                    case 3:
+                        newX++
+                    case 4:
+                        newX--
+                    }
+                }
+            case "erratic":
+                Direction2 := DiceRoll(4)
+                if DiceRoll(2) == 1 {
+                    Direction = Direction2
+                }
+                switch Direction {
+                case 1:
+                    newY++
+                case 2:
+                    newY--
+                case 3:
+                    newX++
+                case 4:
+                    newX--
+                }
+
+            case "rooted":
+                continue
+            case "charge":
+                closest := party.Members[0]
+                closestDist := math.MaxInt32
+                for _, member := range party.Members {
+                    distX := member.X - crit.X
+                    distY := member.Y - crit.Y
+                    dist := distX*distX + distY*distY
+                    if dist < closestDist {
+                        closestDist = dist
+                        closest = member
+                    }
+                }
+                if closest.X > crit.X { newX++ } else if closest.X < crit.X { newX-- }
+                if closest.Y > crit.Y { newY++ } else if closest.Y < crit.Y { newY-- }
+        }
+            if newX >= 0 && newX < len(grid[0]) && newY >= 0 && newY < len(grid) {
+        if grid[newY][newX].Walkable && grid[newY][newX].Entity == nil {
+            grid[crit.Y][crit.X].Entity = nil
+            crit.X = newX
+            crit.Y = newY
+            grid[crit.Y][crit.X].Entity = crit
+        }
+    }
+    }  // ← for loop closes here
+}
+
+
+func (cm *CritterManager) CheckAdjacency(party *Party) []string {
+    messages := []string{}
+    for _, member := range party.Members {
+        for _, crit := range cm.Critters {
+            distX := crit.X - member.X
+            distY := crit.Y - member.Y
+            if distX < 0 { distX = -distX } // absolute value
+            if distY < 0 { distY = -distY }
+            adjacent := distX <= 1 && distY <= 1
+
+            if adjacent {
+            if crit.Hostile {
+                messages = append(messages, fmt.Sprintf("⚔️ %s attacks %s!", crit.Name, member.Name))
+            } else {
+                messages = append(messages, fmt.Sprintf("✨ %s encounters %s!", member.Name, crit.Name))
+            }
+            }
+        }
+    }
+    return messages
+}
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
     grid := game.GenerateGrid(30, 20)
     party := game.NewParty(grid)
     j := game.NewJourney()
+    cm := game.NewCritterManager()
 
     // Hide cursor for cleaner rendering
     fmt.Print("\033[?25l")
@@ -19,8 +20,17 @@ func main() {
     // Clear screen once at the start
     fmt.Print("\033[2J")
 
+    cm.Spawn(game.RandomCritter(), grid)
+    cm.Spawn(game.RandomCritter(), grid)
+    cm.Spawn(game.RandomCritter(), grid)
+
     for {
         game.DrawGrid(grid, j, party)
+        cm.MoveAll(grid, party)
+        messages := cm.CheckAdjacency(party)
+        for _, msg := range messages {
+            fmt.Println(msg)
+        }
         
         cx, cy := party.Center()
         for _, character := range party.Members {
